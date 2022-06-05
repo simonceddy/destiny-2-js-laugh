811ee5fa4b9fafd1cd3644fba8728b0276336291
Simon Eddy <simon@simoneddy.com.au>
2022-06-06 02:14:09 +1000

    removed unused util

--- src/util/url/makeOauthUrl.js
+++
@@ -1,7 +0,0 @@
-function makeOauthUrl(destiny, clientId) {
-  const url = `${destiny.oauthConfig.url}?client_id=${clientId}&response_type=code`;
-
-  return url;
-}
-
-module.exports = makeOauthUrl;
