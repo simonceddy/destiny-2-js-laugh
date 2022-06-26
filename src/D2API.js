24bbcfe67e1770abdf022824d8c8c803270d13ed
Simon Eddy <simon@simoneddy.com.au>
2022-06-26 15:09:02 +1000

    switch to massive json db

--- src/D2API.js
+++ src/D2API.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('node:https');
-const SZIP = require('node-stream-zip');
+// const SZIP = require('node-stream-zip');
 const D2 = require('node-destiny-2/lib/destiny-2-api');
 // const promiseRequest = require('node-destiny-2/lib/async-https');
 
@@ -20,30 +20,32 @@ class D2API extends D2 {
     if (!this.dbPath) {
       const { Response } = await this.getManifest();
       if (!Response) { console.error('error!'); } else {
-        this.dbPath = Response.mobileWorldContentPaths.en;
+        this.dbPath = Response.jsonWorldContentPaths.en;
       }
     }
     const opts = { ...this.options, path: this.dbPath, method: 'GET' };
-    const zipPath = path.resolve(__dirname, '../storage/current.zip');
-    const body = fs.createWriteStream(zipPath);
+    const zipPath = path.resolve(__dirname, '../storage/current.json');
+    const chunks = [];
     const req = https.request(opts, (res) => {
       console.log(`STATUS: ${res.statusCode}`);
       console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
       res.setEncoding('utf8');
-      res.on('data', () => {
-        console.log('data received');
-      }).pipe(body);
+      res.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
       res.on('end', () => {
         console.log('No more data in response.');
-        const zip = new SZIP({
-          file: zipPath,
-          storeEntries: true
-        });
-        zip.on('ready', () => zip.extract(
-          this.dbPath,
-          path.resolve(__dirname, '../storage/manifest.content'),
-          (err) => console.error(err)
-        ));
+        fs.writeFileSync(zipPath, chunks.join());
+        // const zip = new SZIP({
+        //   file: zipPath,
+        //   storeEntries: true
+        // });
+        // zip.on('error', (err) => console.error(err));
+        // zip.on('ready', () => zip.extract(
+        //   this.dbPath,
+        //   path.resolve(__dirname, '../storage/manifest.content'),
+        //   (err) => console.error(err)
+        // ));
       });
     });
     req.on('response', () => console.log('response received'));
